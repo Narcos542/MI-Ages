bf01b6448fd6095d57a1f597db720e60d74580be
Kisuny <homeca19@gmail.com>
2025-05-12 12:26:50 +0300

    hide furnaces

--- kubejs/client_scripts/jei/hide_items.js
+++ kubejs/client_scripts/jei/hide_items.js
@@ -1,3 +1,9 @@
 RecipeViewerEvents.removeEntries('item', event => {
-	event.remove('ytech:tin_ingot')
+	event.remove([
+		'ytech:tin_ingot',
+		'modern_industrialization:electric_furnace', 
+		'modern_industrialization:steel_furnace', 
+		'modern_industrialization:bronze_furnace'
+	])
+ 
 })
