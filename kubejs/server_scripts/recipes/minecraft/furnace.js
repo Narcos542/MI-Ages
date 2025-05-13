52721440806ba77df6513f314066957e66266cce
Kisuny <homeca19@gmail.com>
2025-05-13 18:23:04 +0300

    Removed the melting of the nuggets in the furnace

---
+++ kubejs/server_scripts/recipes/minecraft/furnace.js
@@ -0,0 +1,11 @@
+ServerEvents.recipes(event => {
+    const remove_by_id = [
+        'minecraft:iron_nugget_from_smelting',
+        /modern_industrialization:materials\/.*\/smelting\/tiny_dust_to_nugget_smelting/
+    ]
+
+    remove_by_id.forEach(id => {
+        event.remove({type: "minecraft:smelting", id: id})
+    });
+    
+})
