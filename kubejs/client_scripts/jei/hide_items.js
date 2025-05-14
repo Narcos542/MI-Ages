857efec99e937d261014c2016a737e2e44bd83c9
Narcos542 <78523240+Narcos542@users.noreply.github.com>
2025-05-15 02:31:44 +1000

    Integration IE with MI (without recipes))
    1) Adding new matterials to MI for replace IE (constantan, plastic, netherite rod and nugget, HOP graphite).
    
    2) Remove ingots, plates, wire, coke oven, blastfurnace wit recipes.
    
    3) Replace MI stell block from IE, this need for IE multiblocks.
    
    4) Remove recipes from arcfurnace and crusher, which break MI progression (Aluminum/tungsten/platinum)
    
    5) Remove hammering recipes shapless
    
    6) Add steel, lead wires for recipe IE
    
    7) Some refactoring and optimization

--- kubejs/client_scripts/jei/hide_items.js
+++ kubejs/client_scripts/jei/hide_items.js
@@ -1,3 +1,23 @@
+// Create constant list for work with regular expression
+const list = [
+	'ytech:tin_ingot',
+	'modern_industrialization:steel_block',
+	'immersiveengineering:ethanol_bucket',
+	'immersiveengineering:biodiesel_bucket',
+	/immersiveengineering:.*coke.*/,
+    /immersiveengineering:plate.*/,
+    /immersiveengineering:wire.*/,
+    /immersiveengineering:stick.*/,
+    /immersiveengineering:dust.*/,
+    /immersiveengineering:ingot.*/,
+    /immersiveengineering:raw.*/,
+    /immersiveengineering:nugget.*/,
+    /immersiveengineering:.*blast.*/
+	
+]
+
+// Hide items
 RecipeViewerEvents.removeEntries('item', event => {
-	event.remove('ytech:tin_ingot')
+
+	event.remove(list)
 })
